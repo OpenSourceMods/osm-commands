e3306108f08d79dcfcc6db6178f819d3b4799e43
Open Source Modifications <94762646+OpenSourceMods@users.noreply.github.com>
2022-11-23 12:03:03 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,19 @@
+-- Made By Open Source Modifications © 2022
+-- Join Our Official Discord for support: https://discord.gg/ptEkC6Ym9w
+-- Thankyou for choosing Open Source Modifications!
 
+fx_version 'cerulean'
+
+games { 'gta5' }
+
+author 'Open Source Modifications'
+
+description 'A simple chat command script'
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
