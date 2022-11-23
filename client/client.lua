1cb7a05031dadf569859e8d3e86989d16b8707db
Open Source Modifications <94762646+OpenSourceMods@users.noreply.github.com>
2022-11-23 12:02:37 +0000

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,1 +1,26 @@
+-- Made By Open Source Modifications © 2022
+-- Join Our Official Discord for support: https://discord.gg/ptEkC6Ym9w
+-- Thankyou for choosing Open Source Modifications!
 
+
+-- Chat Suggestion Threads
+Citizen.CreateThread(function()
+	TriggerEvent('chat:addSuggestion', '/advert', 'Creates an advertisement!', {
+		{name = 'Message', help = 'Enter a Message!'},
+	})
+	TriggerEvent('chat:addSuggestion', '/twitter', 'Creates an tweet!', {
+		{name = 'Message', help = 'Enter a Message!'},
+	})
+	TriggerEvent('chat:addSuggestion', '/me', 'Creates a Me message!', {
+		{name = 'Message', help = 'Enter a Message!'},
+	})
+	TriggerEvent('chat:addSuggestion', '/darkweb', 'Creates a secret DARKWEB Message!', {
+		{name = 'Message', help = 'Enter a Message!'},
+	})
+	TriggerEvent('chat:addSuggestion', '/do', 'Creates a local DO Message!', {
+		{name = 'Message', help = 'Enter a Message!'},
+	})
+    TriggerEvent('chat:addSuggestion', '/ooc', 'Creates a an ooc Message!', {
+		{name = 'Message', help = 'Enter a Message!'},
+	})
+  end)
